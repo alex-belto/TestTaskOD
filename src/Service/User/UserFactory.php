8d983fb6359a97d4f8ccabb30c03a43edb1fcb17
Alex <tyazhkorob.aleks@icloud.com>
2023-11-11 16:01:10 +0200

    made an authentication create User and UserFactory create user fixtures

---
+++ src/Service/User/UserFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service\User;
+
+use App\Entity\User;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFactory
+{
+    private UserPasswordHasherInterface $hasher;
+    public function __construct(UserPasswordHasherInterface $hasher) {
+        $this->hasher = $hasher;
+    }
+    public function createAdmin(): User
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setRoles(['ROLE_ADMIN']);
+        $password = $this->hasher->hashPassword($user, 'admin');
+        $user->setPassword($password);
+
+        return $user;
+    }
+
+    public function createUser(): User
+    {
+        $faker = Factory::create();
+        $user = new User();
+        $user->setUsername($faker->userName());
+        $user->setRoles(['ROLE_USER']);
+        $password = $this->hasher->hashPassword($user, 'user');
+        $user->setPassword($password);
+
+        return $user;
+    }
+
+}
